5c1c7615cc200f52c11a59997192a07ba14f7720
Martin Wernstål <m4rw3r@gmail.com>
2015-08-14 05:52:32 +0200

    Grouped monad and functor into separate modules

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,17 @@ pub use combinators::{
     try,
 };
 
+pub use functor::map;
+
 pub use iter::{
     Iter,
     IResult,
+};
+
+pub use monad::{
+    bind,
+    err,
+    ret,
 };
 
 pub use parsers::{
@@ -126,73 +134,105 @@ impl<'a, I: 'a + Copy + fmt::Debug, T, E: error::Error> Parser<'a, I, T, E> {
     }
 }
 
-// Functor
+mod functor {
+    use ::Parser;
 
-impl<'a, I: 'a + Copy, T, E> Parser<'a, I, T, E> {
+    impl<'a, I: 'a + Copy, T, E> Parser<'a, I, T, E> {
+        /// Applies the function ``f`` to the wrapped value if the parser is in a success state, otherwise
+        /// does nothing.
+        /// 
+        /// ```
+        /// use parser::{Error, Parser, ret};
+        /// 
+        /// let p: Parser<_, usize, Error<()>> = ret(From::from(b"dummy"), 1234);
+        /// 
+        /// assert_eq!(p.map(|i| i + 20).unwrap(), 1254);
+        /// ```
+        #[inline]
+        pub fn map<F, U>(self, f: F) -> Parser<'a, I, U, E>
+          where F: FnOnce(T) -> U {
+            Parser(self.0, self.1.map(f))
+        }
+    }
+
     /// Applies the function ``f`` to the wrapped value if the parser is in a success state, otherwise
     /// does nothing.
     /// 
     /// ```
-    /// use parser::{Error, Parser, ret};
+    /// use parser::{Error, Parser, map, ret};
     /// 
     /// let p: Parser<_, usize, Error<()>> = ret(From::from(b"dummy"), 1234);
     /// 
-    /// assert_eq!(p.map(|i| i + 20).unwrap(), 1254);
+    /// assert_eq!(map(p, |i| i + 20).unwrap(), 1254);
     /// ```
     #[inline]
-    pub fn map<F, U>(self, f: F) -> Parser<'a, I, U, E>
+    pub fn map<'a, I: 'a + Copy, T, E, F, U>(m: Parser<'a, I, T, E>, f: F) -> Parser<'a, I, U, E>
       where F: FnOnce(T) -> U {
-        Parser(self.0, self.1.map(f))
+        m.map(f)
     }
 }
 
-/// Applies the function ``f`` to the wrapped value if the parser is in a success state, otherwise
-/// does nothing.
-/// 
-/// ```
-/// use parser::{Error, Parser, map, ret};
-/// 
-/// let p: Parser<_, usize, Error<()>> = ret(From::from(b"dummy"), 1234);
-/// 
-/// assert_eq!(map(p, |i| i + 20).unwrap(), 1254);
-/// ```
-#[inline]
-pub fn map<'a, I: 'a + Copy, T, E, F, U>(m: Parser<'a, I, T, E>, f: F) -> Parser<'a, I, U, E>
-  where F: FnOnce(T) -> U {
-    m.map(f)
-}
+mod monad {
+    use ::Empty;
+    use ::State;
+    use ::Parser;
 
-// Monad
+    /// Applies the function ``f`` on the value in the parser ``m``, the first parameter to ``f`` is
+    /// the context of the parser, to be used for continued processing and the second parameter is the
+    /// value contained in ``m``.
+    #[inline]
+    pub fn bind<'a, I: 'a + Copy, T, E, F, U, O>(m: Parser<'a, I, T, E>, f: F) -> Parser<'a, I, U, O>
+      where O: From<E>,
+            F: FnOnce(Empty<'a, I>, T) -> Parser<'a, I, U, O> {
+        match m.1 {
+            State::Ok(s) => {
+                let r = f(Parser(m.0, State::Ok(())), s);
 
-/// Applies the function ``f`` on the value in the parser ``m``, the first parameter to ``f`` is
-/// the context of the parser, to be used for continued processing and the second parameter is the
-/// value contained in ``m``.
-#[inline]
-pub fn bind<'a, I: 'a + Copy, T, E, F, U, O>(m: Parser<'a, I, T, E>, f: F) -> Parser<'a, I, U, O>
-  where O: From<E>,
-        F: FnOnce(Empty<'a, I>, T) -> Parser<'a, I, U, O> {
-    match m.1 {
-        State::Ok(s) => {
-            let r = f(Parser(m.0, State::Ok(())), s);
-
-            // We rollback if the parser ``f`` failed
-            Parser(if r.1.is_good() { r.0 } else { m.0 }, r.1.map_err(From::from))
-        },
-        State::Err(i, e)        => Parser(m.0, State::Err(i, From::from(e))),
-        State::Incomplete(i, r) => Parser(m.0, State::Incomplete(i, r))
+                // We rollback if the parser ``f`` failed
+                Parser(if r.1.is_good() { r.0 } else { m.0 }, r.1.map_err(From::from))
+            },
+            State::Err(i, e)        => Parser(m.0, State::Err(i, From::from(e))),
+            State::Incomplete(i, r) => Parser(m.0, State::Incomplete(i, r))
+        }
     }
-}
 
-/// Constructs a success value from the given ``value``.
-#[inline]
-pub fn ret<'a, I: 'a + Copy, T, E>(m: Empty<'a, I>, value: T) -> Parser<'a, I, T, E> {
-    Parser(m.0, State::Ok(value))
-}
+    /// Constructs a success value from the given ``value``.
+    /// 
+    /// Currently this function may need type-annotation for the third type-parameter ``E`` because
+    /// the error-value is unconstrained by this function. When wrapped in smaller functions
+    /// inference should be able to use the return-value of the function to infer return and error
+    /// types.
+    /// 
+    /// ```
+    /// use parser::{Parser, Error, ret};
+    /// 
+    /// let p = From::from(b"test");
+    /// 
+    /// assert_eq!(ret::<_, _, Error<u8>>(p, "data").unwrap(), "data");
+    /// ```
+    #[inline]
+    pub fn ret<'a, I: 'a + Copy, T, E>(m: Empty<'a, I>, value: T) -> Parser<'a, I, T, E> {
+        Parser(m.0, State::Ok(value))
+    }
 
-/// Constructs an error value from the given error ``err``.
-#[inline]
-pub fn err<'a, I: 'a + Copy, T, E>(m: Empty<'a, I>, err: E) -> Parser<'a, I, T, E> {
-    Parser(m.0, State::Err(m.0, err))
+    /// Constructs an error value from the given error ``err``.
+    /// 
+    /// Currently this function may need type-annotation for the second type-parameter ``T`` because
+    /// the success-value is unconstrained by this function. When wrapped in smaller functions
+    /// inference should be able to use the return-value of the function to infer return and error
+    /// types.
+    /// 
+    /// ```should_panic
+    /// use parser::{Parser, Error, err};
+    /// 
+    /// let p = From::from(b"test");
+    /// 
+    /// assert_eq!(err::<_, &[u8], _>(p, Error::Unexpected(2)).unwrap(), b"data");
+    /// ```
+    #[inline]
+    pub fn err<'a, I: 'a + Copy, T, E>(m: Empty<'a, I>, err: E) -> Parser<'a, I, T, E> {
+        Parser(m.0, State::Err(m.0, err))
+    }
 }
 
 /// Constructs a parser monad for parsing the data in the buffer supplied to ``From::from()``.
