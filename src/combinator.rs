b37a419b6f239fe72aa755488ff027f44d09ee15
Martin Wernstål <m4rw3r@gmail.com>
2015-08-25 17:36:52 +0200

    Removed older iterator, no performance difference

--- src/combinator.rs
+++ src/combinator.rs
@@ -1,17 +1,10 @@
 use std::iter::FromIterator;
 
-#[cfg(not(feature = "verbose_error"))]
-use error;
-
 use Input;
 use Parser;
 use State;
 
-#[cfg(not(feature = "verbose_error"))]
-use Error;
-
 use iter::Iter;
-use iter::IResult;
 
 /// Tries to match the parser ``f``, if ``f`` fails it tries ``g``. Returns the success value of
 /// the first match, otherwise the error of the last one if both fail.
@@ -60,49 +53,6 @@ pub fn or<'a, I, T, E, F, G>(m: Input<'a, I>, f: F, g: G) -> Parser<'a, I, T, E>
 /// assert_eq!(v[0], b"a");
 /// assert_eq!(v[1], b"bc");
 /// ```
-#[cfg(not(feature = "verbose_error"))]
-#[inline]
-pub fn many<'a, I, T, E, F, U>(m: Input<'a, I>, f: F) -> Parser<'a, I, T, E>
-  where I: Copy,
-        F: Fn(Input<'a, I>) -> Parser<'a, I, U, E>,
-        T: FromIterator<U> {
-    let mut iter = Iter::new(m.0, f);
-
-    let result: T = FromIterator::from_iter(iter.by_ref());
-
-    match iter.last_state() {
-        // We haven't read everything yet
-        IResult::Good       => Parser(State::Incomplete(1)),
-        // Ok, last parser failed, we have iterated all.
-        // Return remainder of buffer and the collected result
-        IResult::Bad        => Parser(State::Item(iter.buffer(), result)),
-        // Nested parser incomplete, propagate
-        IResult::Incomplete => Parser(State::Incomplete(1)),
-    }
-}
-
-/// Parses many instances of ``f`` until it does no longer match, returning all matches.
-/// 
-/// Note: If the last parser succeeds on the last input item then this parser is still considered
-/// incomplete as there might be more data to fill.
-/// 
-/// Note: Allocates data.
-/// 
-/// ```
-/// use parser::{Error, Parser, bind, char, many, ret, take_while1};
-/// 
-/// let p = From::from(b"a,bc,cd ");
-/// 
-/// let r: Parser<_, Vec<&[u8]>, Error<_>> = many(p, |m| bind(
-///     take_while1(m, |c| c != b',' && c != b' '), |m, c| bind(
-///         char(m, b','), |m, _| ret(m, c))));
-/// let v = r.unwrap();
-///
-/// assert_eq!(v.len(), 2);
-/// assert_eq!(v[0], b"a");
-/// assert_eq!(v[1], b"bc");
-/// ```
-#[cfg(feature = "verbose_error")]
 #[inline]
 pub fn many<'a, I, T, E, F, U>(m: Input<'a, I>, f: F) -> Parser<'a, I, T, E>
   where I: Copy,
@@ -140,53 +90,9 @@ pub fn many<'a, I, T, E, F, U>(m: Input<'a, I>, f: F) -> Parser<'a, I, T, E>
 ///     take_while1(m, |c| c != b',' && c != b' '), |m, c| bind(
 ///         char(m, b','), |m, _| ret::<_, _, Error<_>>(m, c))));
 ///
-/// let _ = r.unwrap();
-/// ```
-// TODO: Proper checking of return value in doc-test
-#[cfg(not(feature = "verbose_error"))]
-#[inline]
-pub fn many1<'a, I, T, E, F, U>(m: Input<'a, I>, f: F) -> Parser<'a, I, T, Error<I>>
-  where I: Copy,
-        F: Fn(Input<'a, I>) -> Parser<'a, I, U, E>,
-        T: FromIterator<U> {
-    // If we have gotten an item, if this is false after from_iter we have failed
-    let mut item = false;
-    let mut iter = Iter::new(m.0, f);
-
-    let result: T = FromIterator::from_iter(iter.by_ref().inspect(|_| item = true ));
-
-    match (item, iter.last_state()) {
-        // We haven't read everything yet
-        (true,  IResult::Good)       => Parser(State::Incomplete(1)),
-        // Ok, last parser failed, we have iterated all
-        (true,  IResult::Bad)        => Parser(State::Item(iter.buffer(), result)),
-        // Nested parser incomplete, propagate
-        (_,     IResult::Incomplete) => Parser(State::Incomplete(1)),
-        (false, _)                   => Parser(State::Error(m.0, error::many1())),
-    }
-}
-
-/// Parses at least one instance of ``f`` and continues until it does no longer match,
-/// returning all matches.
-/// 
-/// Note: If the last parser succeeds on the last input item then this parser is still considered
-/// incomplete as there might be more data to fill.
-/// 
-/// Note: Allocates data.
-/// 
-/// ```should_panic
-/// use parser::{Error, Parser, bind, char, many1, ret, take_while1};
-///
-/// let p = From::from(b"a");
-///
-/// let r: Parser<_, Vec<&[u8]>, Error<_>> = many1(p, |m| bind(
-///     take_while1(m, |c| c != b',' && c != b' '), |m, c| bind(
-///         char(m, b','), |m, _| ret::<_, _, Error<_>>(m, c))));
-///
 /// let v = r.unwrap();
 /// ```
 // TODO: Proper checking of return value in doc-test
-#[cfg(feature = "verbose_error")]
 #[inline]
 pub fn many1<'a, I, T, E, F, U>(m: Input<'a, I>, f: F) -> Parser<'a, I, T, E>
   where I: Copy,
--- src/iter.rs
+++ src/iter.rs
@@ -1,148 +1,62 @@
-pub use self::wrap::{Iter, IResult};
+use std::marker::PhantomData;
 
-#[cfg(not(feature = "verbose_error"))]
-mod wrap {
-    use std::marker::PhantomData;
+use ::{
+    Input,
+    Parser,
+    State,
+};
 
-    use ::{
-        Input,
-        Parser,
-        State,
-    };
+pub struct Iter<'a, I, U, E, F>
+  where I: 'a,
+        F: FnMut(Input<'a, I>) -> Parser<'a, I, U, E> {
+    s: State<'a, I, (), E>,
+    f: F,
+    b: &'a [I],
+    u: PhantomData<U>,
+}
 
-    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-    // TODO: Remove this from public
-    pub enum IResult {
-        Good,
-        Bad,
-        Incomplete,
-    }
-
-    // TODO: Needs some better way of iterating items
-    pub struct Iter<'a, I, U, E, F>
-      where I: 'a,
-            F: FnMut(Input<'a, I>) -> Parser<'a, I, U, E> {
-        /// Last resulting state
-        l: IResult,
-        /// Current buffer
-        b: &'a [I],
-        f: F,
-        u: PhantomData<U>,
-        e: PhantomData<E>,
-    }
-
-    impl<'a, I: 'a + Copy, T, E, F> Iter<'a, I, T, E, F>
-      where F: FnMut(Input<'a, I>) -> Parser<'a, I, T, E> {
-        #[inline]
-        pub fn new(buffer: &'a [I], f: F) -> Iter<'a, I, T, E, F> {
-            Iter{
-                l: IResult::Good,
-                b: buffer,
-                f: f,
-                u: PhantomData,
-                e: PhantomData
-            }
-        }
-
-        #[inline]
-        pub fn last_state(&self) -> IResult {
-            self.l
-        }
-
-        #[inline]
-        pub fn buffer(&self) -> &'a [I] {
-            self.b
+impl<'a, I: 'a + Copy, T, E, F> Iter<'a, I, T, E, F>
+  where F: FnMut(Input<'a, I>) -> Parser<'a, I, T, E> {
+    #[inline]
+    pub fn new(buffer: &'a [I], f: F) -> Iter<'a, I, T, E, F> {
+        Iter{
+            s: State::Item(buffer, ()),
+            f: f,
+            b: buffer,
+            u: PhantomData,
         }
     }
 
-    impl<'a, I: 'a + Copy, T, E, F> Iterator for Iter<'a, I, T, E, F>
-      where F: FnMut(Input<'a, I>) -> Parser<'a, I, T, E> {
-        type Item = T;
-
-        #[inline]
-        fn next(&mut self) -> Option<Self::Item> {
-            if self.l != IResult::Good {
-                return None
-            }
-
-            match (self.f)(Input(self.b)).0 {
-                State::Item(b, v)    => {
-                    self.l = IResult::Good;
-                    self.b = b;
-
-                    Some(v)
-                },
-                State::Error(_, _)   => { self.l = IResult::Bad;        None },
-                State::Incomplete(_) => { self.l = IResult::Incomplete; None },
-            }
-        }
+    #[inline]
+    pub fn state(self) -> (&'a [I], State<'a, I, (), E>) {
+        (self.b, self.s)
     }
 }
 
-#[cfg(feature = "verbose_error")]
-mod wrap {
-    use std::marker::PhantomData;
+impl<'a, I, T, E, F> Iterator for Iter<'a, I, T, E, F>
+  where I: 'a,
+        F: FnMut(Input<'a, I>) -> Parser<'a, I, T, E> {
+    type Item = T;
 
-    use ::{
-        Input,
-        Parser,
-        State,
-    };
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        match (self.f)(Input(self.b)) {
+            Parser(State::Item(b, v)) => {
+                self.b = b;
+                self.s = State::Item(b, ());
 
-    pub struct IResult;
+                Some(v)
+            },
+            Parser(State::Error(b, e)) => {
+                self.s = State::Error(b, e);
 
-    pub struct Iter<'a, I, U, E, F>
-      where I: 'a,
-            F: FnMut(Input<'a, I>) -> Parser<'a, I, U, E> {
-        s: State<'a, I, (), E>,
-        f: F,
-        b: &'a [I],
-        u: PhantomData<U>,
-    }
+                None
+            },
+            Parser(State::Incomplete(n)) => {
+                self.s = State::Incomplete(n);
 
-    impl<'a, I: 'a + Copy, T, E, F> Iter<'a, I, T, E, F>
-      where F: FnMut(Input<'a, I>) -> Parser<'a, I, T, E> {
-        #[inline]
-        pub fn new(buffer: &'a [I], f: F) -> Iter<'a, I, T, E, F> {
-            Iter{
-                s: State::Item(buffer, ()),
-                f: f,
-                b: buffer,
-                u: PhantomData,
-            }
-        }
-
-        #[inline]
-        pub fn state(self) -> (&'a [I], State<'a, I, (), E>) {
-            (self.b, self.s)
-        }
-    }
-
-    impl<'a, I, T, E, F> Iterator for Iter<'a, I, T, E, F>
-      where I: 'a,
-            F: FnMut(Input<'a, I>) -> Parser<'a, I, T, E> {
-        type Item = T;
-
-        #[inline]
-        fn next(&mut self) -> Option<Self::Item> {
-            match (self.f)(Input(self.b)) {
-                Parser(State::Item(b, v)) => {
-                    self.b = b;
-                    self.s = State::Item(b, ());
-
-                    Some(v)
-                },
-                Parser(State::Error(b, e)) => {
-                    self.s = State::Error(b, e);
-
-                    None
-                },
-                Parser(State::Incomplete(n)) => {
-                    self.s = State::Incomplete(n);
-
-                    None
-                },
-            }
+                None
+            },
         }
     }
 }
