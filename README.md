7572e44ff655414742a263767593067120e5fbd9
Martin Wernstål <m4rw3r@gmail.com>
2015-09-05 16:26:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This branch uses [eddyb's rust fork](https://github.com/eddyb/rust/commits/calendar-driven-development)
+which supports anonymous return types.
---
+++ src/lib.rs
@@ -0,0 +1,127 @@
+#![feature(default_type_parameter_fallback)]
+
+// pub type Parser<I, T, E> = for<'a> FnOnce(&'a [I]) -> State<'a, I, T, E>;
+
+/*
+trait Parser<'a, I, T, E> {
+    fn parse(self, &'a [I]) -> State<'a, I, T, E>;
+}
+
+impl<I, T, E, F> Parser<I, T, E> for F
+  where F: for<'a> Fn(&'a [I]) -> State<'a, I, T, E> {
+    fn parse<'a>(&self, i: &'a [I]) -> State<'a, I, T, E> {
+        self(i)
+    }
+}
+
+impl<I, T, E, F> Parser<I, T, E> for F
+  where F: for<'a> FnMut(&'a [I]) -> State<'a, I, T, E> {
+    fn parse<'a>(&self, i: &'a [I]) -> State<'a, I, T, E> {
+        self(i)
+    }
+}*/
+
+#[derive(Debug, Eq, PartialEq)]
+#[must_use]
+pub enum State<'a, I, T, E>
+  where I: 'a {
+    /// Success state, slice is the slice of the input data starting just after the last successful
+    /// parser.
+    Item(&'a [I], T),
+    /// Error state, slice is the slice of the input data from the position where the error
+    /// occurred.
+    Error(&'a [I], E),
+    // Do not include data-slice here as we do not need to actually return the part which is
+    // incomplete. The parse has to restart anyway from the index where it was.
+    Incomplete(usize),
+}
+
+pub mod monad {
+    use ::Parser;
+    use ::State;
+
+    pub fn bind<'a, I, P, T, E, F, U, R, V = E>(p: P, f: F) -> impl for<'p>FnOnce(&'p [I]) -> State<'p, I, U, V> + 'a
+      where P: for<'p>FnOnce(&'p [I]) -> State<'p, I, T, E> + 'a,
+            F: FnOnce(T) -> R + 'a,
+            R: for<'p>FnOnce(&'p [I]) -> State<'p, I, U, V>,
+            V: From<E> {
+        move |i| {
+            match p(i) {
+                State::Item(b, t)    => f(t)(b),
+                State::Error(b, e)   => State::Error(b, From::from(e)),
+                State::Incomplete(n) => State::Incomplete(n),
+            }
+        }
+    }
+
+    pub fn ret<'a, I, T, E = ()>(a: T) -> impl for<'p>FnOnce(&'p [I]) -> State<'p, I, T, E> + 'a
+      where T: 'a {
+        move |i| {
+            State::Item(i, a)
+        }
+    }
+
+    pub fn err<'a, I, T, E>(e: E) -> impl for<'p>FnOnce(&'p [I]) -> State<'p, I, T, E> + 'a
+      where E: 'a {
+        move |i| {
+            State::Error(i, e)
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use ::State;
+        use ::monad::{bind, ret, err};
+
+        #[test]
+        fn left_identity() {
+            fn f<I>(i: u32) -> impl for<'p> FnOnce(&'p [I]) -> State<'p, I, u32, ()>
+              where I: Copy {
+                ret(i + 1)
+            }
+
+            let a = 123;
+            // return a >>= f
+            let lhs = bind(ret(a), f);
+            // f a
+            let rhs = f(a);
+
+            assert_eq!(lhs(b"test"), State::Item(&b"test"[..], 124));
+            assert_eq!(rhs(b"test"), State::Item(&b"test"[..], 124));
+        }
+
+        #[test]
+        fn right_identity() {
+            let m1 = ret::<_, usize, ()>(1);
+            let m2 = ret::<_, usize, ()>(1);
+
+            let lhs = bind(m1, ret);
+            let rhs = m2;
+
+            assert_eq!(lhs(b"test"), State::Item(&b"test"[..], 1));
+            assert_eq!(rhs(b"test"), State::Item(&b"test"[..], 1));
+        }
+        
+        #[test]
+        fn associativity() {
+             fn f<I: Copy>(num: u32) -> impl for<'p> FnOnce(&'p [I]) -> State<'p, I, u64, ()> {
+                ret((num + 1) as u64)
+            }
+
+            fn g<I: Copy>(num: u64) -> impl for<'p> FnOnce(&'p [I]) -> State<'p, I, u64, ()> {
+                ret(num * 2)
+            }
+
+            let lhs_m = ret::<_, _, ()>(2);
+            let rhs_m = ret::<_, _, ()>(2);
+
+            // (m >>= f) >>= g
+            let lhs = bind(bind(lhs_m, f), g);
+            // m >>= (\x -> f x >> g)
+            let rhs = bind(rhs_m, |x| bind(f(x), g));
+
+            assert_eq!(lhs(b"test"), State::Item(&b"test"[..], 6));
+            assert_eq!(rhs(b"test"), State::Item(&b"test"[..], 6));
+        }
+    }
+}
